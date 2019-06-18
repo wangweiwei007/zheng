dda58f32fe08a538b0a2279a10ad6c766ed96680
wangweiwei <784264288@qq.com>
2019-06-18 19:36:38 +0800

    增加应用服务启动配置

---
+++ zheng-upms/zheng-upms-server/deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#编译+部署jenkins-test站点
+
+#需要配置如下参数
+# 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+
+### base 函数
+killTomcat()
+{
+    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no tomcat pid alive"
+    else
+      kill -9 $pid
+    fi
+}
+cd $PROJ_PATH/zheng-upms/zheng-upms-server
+mvn clean install -Dmaven.test.skip=true
+
+# 停tomcat
+killTomcat
+
+#备份原有工程
+cp $TOMCAT_APP_PATH/webapps/zheng-upms-server.war  $TOMCAT_APP_PATH/backup/zheng-upms-server.war.`date +%Y%m%d%H%M`
+
+# 删除原有工程
+rm -rf $TOMCAT_APP_PATH/webapps/*
+
+# 复制新的工程
+cp $PROJ_PATH/zheng-upms/zheng-upms-server/target/zheng-upms-server.war $TOMCAT_APP_PATH/webapps/
+
+# cd $TOMCAT_APP_PATH/webapps/
+# mv zheng-upms-server.war zheng-upms-server.war
+
+# 启动Tomcat
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
